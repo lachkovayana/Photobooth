f63856990dc94b76717879f28342eb14c632210d
lachkovayana <45456220+lachkovayana@users.noreply.github.com>
2022-02-06 22:02:17 +0700

    added horizontal reflection

--- app/js/photobooth.mjs
+++ app/js/photobooth.mjs
@@ -37,14 +37,17 @@ export class Photobooth {
   clear() {
     this._gallery.clear()
   }
-
-  _shot() {
-    this.ctx = this.canvas.getContext('2d');
+  
+ _shot() {
     this.canvas.width = this.video.videoWidth;
     this.canvas.height = this.video.videoHeight;
+
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.translate(this.canvas.width, 0);
+    this.ctx.scale(-1, 1);
     this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
 
-    var data = this.canvas.toDataURL('image/png');
+    let data = this.canvas.toDataURL('image/png');
     this._gallery.addPicture(data)
   }
 
